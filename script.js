c99c00e38b8123b834a4d9aa883b1d72efe5c7f7
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-06-20 20:30:14 -0400

    Dashboard

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.mobile-nav').classList.toggle('display')
+});
+
+
+
+// const click = document.querySelector('.bot');
+// click.addEventListener("click", ()=>{
+//     document.querySelector('aside').classList.toggle('aside-after')
+// })
--- script.jsx
+++
@@ -1,3 +0,0 @@
-document.querySelector('.hamburger').addEventListener('click', ()=>{
-    document.querySelector('.mobile-nav').classList.toggle('display')
-})
